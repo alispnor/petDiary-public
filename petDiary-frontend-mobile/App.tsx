c9dfa049f03d037b3baf77b172e011fcf06feff5
ali <alisalahi@guep.com.br>
2026-04-06 16:53:57 -0300

    ajuste de mobile expo

--- petDiary-frontend-mobile/App.tsx
+++ petDiary-frontend-mobile/App.tsx
@@ -1,23 +1,13 @@
 import React from 'react';
-import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet } from 'react-native';
 import { AppNavigator } from './src/navigation/AppNavigator';
 
 export default function App() {
   return (
-    <GestureHandlerRootView style={styles.container}>
-      <SafeAreaProvider>
-        <StatusBar style="auto" />
-        <AppNavigator />
-      </SafeAreaProvider>
-    </GestureHandlerRootView>
+    <SafeAreaProvider>
+      <StatusBar style="auto" />
+      <AppNavigator />
+    </SafeAreaProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
